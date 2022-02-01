9f681bcdece0c73191526b8d186f0c864d36162d
Fons Politie <fons.drost@Fonss-MacBook-Pro.local>
2022-02-01 16:58:06 +0100

    small adjustment to shell script

--- mac-workstation.sh
+++ mac-workstation.sh
@@ -1,7 +1,9 @@
 #/bin/bash
+chsh -s /bin/bash
 /Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
 pip3 install ansible
-echo "export PATH=$PATH:/Users/fons.drost/Library/Python/3.8/bin/" >> ~/.bash_profile
+export PATH=$PATH:/Users/$USER/Library/Python/3.8/bin/
+echo "export PATH=\$PATH:/Users/$USER/Library/Python/3.8/bin/" >> ~/.bash_profile
 ansible-galaxy collection install community.general
 
 ansible-playbook workstation.yml --e distribution=mac
