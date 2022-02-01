1ea5a9ae235e4916ab99945fc90fc6f9267ae6de
Fons Politie <fons.drost@Fonss-MacBook-Pro.local>
2022-02-01 17:43:05 +0100

    moved some things around

--- mac-workstation.sh
+++ mac-workstation.sh
@@ -1,10 +1,10 @@
 #/bin/bash
-#chsh -s /bin/bash
-#/Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
-#pip3 install ansible
-#export PATH=$PATH:/Users/$USER/Library/Python/3.8/bin/
-#echo "export PATH=\$PATH:/Users/$USER/Library/Python/3.8/bin/" >> ~/.bash_profile
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+chsh -s /bin/bash
+/Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
+pip3 install ansible
+export PATH=$PATH:/Users/$USER/Library/Python/3.8/bin/
+echo "export PATH=\$PATH:/Users/$USER/Library/Python/3.8/bin/" >> ~/.bash_profile
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 ansible-galaxy collection install community.general
 
 ansible-playbook workstation.yml --extra-vars "os=mac user=$USER" --ask-become-pass
