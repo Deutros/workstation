e210db38d0a894e83923d79103b05e2638c82d3d
Fons Politie <fons.drost@Fonss-MacBook-Pro.local>
2022-02-07 17:35:20 +0100

    troubleshooting

--- mac-workstation.sh
+++ mac-workstation.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 # Upgrade PIP and Install Ansible
-/Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
-pip3 install ansible
+#/Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
+#pip3 install ansible
 
 # Make sure we can run Ansible
-export PATH=/Users/$USER/Library/Python/3.8/bin/:$PATH
+#export PATH=/Users/$USER/Library/Python/3.8/bin/:$PATH
 
 # Prepare and run Ansible
 ansible-galaxy collection install community.general
-ansible-playbook workstation.yml --extra-vars "os=mac user=$USER"
+ansible-playbook workstation.yml --extra-vars "os=mac user=$USER" --ask-become-pass
