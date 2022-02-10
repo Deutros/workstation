fcf5343696539acf215a9d15f7d6f9d3260aa76d
Fons Politie <fons.drost@MacBook-Pro-van-Fons.local>
2022-02-10 20:47:38 +0100

    First working version

--- mac-workstation.sh
+++ mac-workstation.sh
@@ -1,10 +1,10 @@
-#/bin/bash
-# Upgrade PIP and Install Ansible
-#/Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
-#pip3 install ansible
+#/bin/zsh
+#Upgrade PIP and Install Ansible
+/Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
+pip3 install ansible
 
 # Make sure we can run Ansible
-#export PATH=/Users/$USER/Library/Python/3.8/bin/:$PATH
+export PATH=/Users/$USER/Library/Python/3.8/bin/:$PATH
 
 # Prepare and run Ansible
 ansible-galaxy collection install community.general
